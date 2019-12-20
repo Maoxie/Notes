ad0d728418ee0930e6b28bdea364a2067d2e9afe
Maoxie <edwardyzt@gmail.com>
2019-12-20 21:08:14 +0800

    update

--- Algorithm/LeetCode/347.前-k-个高频元素.py
+++ Algorithm/LeetCode/347.前-k-个高频元素.py
@@ -50,7 +50,9 @@
 # @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+        counts = {}
+        for n in nums:
+            counts[n] = counts.get(n, 0) + 1
 
 # @lc code=end
 
