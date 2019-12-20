3826636b04ff51ee598dbe3bb1d600c843855db7
Maoxie <edwardyzt@gmail.com>
2019-12-20 23:08:34 +0800

    leetcode 347

--- Algorithm/LeetCode/347.前-k-个高频元素.py
+++ Algorithm/LeetCode/347.前-k-个高频元素.py
@@ -44,15 +44,25 @@
 第一步，确定每个整数出现的次数，可以简单地用一个字典对象维护每个值出现的次数。
 遍历数组，每个数字出现时就给它的计数+1。
 
-第二步，
+第二步，将每个整数出现的次数构建一个大根堆，依次取出堆的前k个元素。
 """
 from typing import List
-from collections import 
+# import heapq
 
 
 # @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return self.solution_3(nums, k)
+
+    def solution_1(self, nums: List[int], k: int) -> List[int]:
+        """
+        自行实现堆结构
+        结果：
+        - 21/21 cases passed (148 ms)
+        - Your runtime beats 37 % of python3 submissions
+        - Your memory usage beats 7.35 % of python3 submissions (17.4 MB)
+        """
         # 第一步：遍历数组，统计整数的出现次数
         # 时间复杂度：O(n)
         counts = {}
@@ -67,12 +77,139 @@ class Solution:
             heap.push(node)
 
         # 第三步：依次取出前k个元素
+        # 时间复杂度：O(logn)
         result = [heap.pop().value for _ in range(k)]
         return result
 
+    def solution_2(self, nums: List[int], k: int) -> List[int]:
+        """
+        用python自带的堆结构
+        结果：
+        - 21/21 cases passed (124 ms)
+        - Your runtime beats 81.98 % of python3 submissions
+        - Your memory usage beats 7.35 % of python3 submissions (17.3 MB)
+        """
+        # 第一步：遍历数组，统计整数的出现次数
+        counts = {}
+        for n in nums:
+            counts[n] = counts.get(n, 0) + 1
+        nodes = (Node(count, num) for num, count in counts.items())
+
+        # 第二步，用python自带的堆结构完成建堆和取前k个值
+        n_largest = heapq.nlargest(k, nodes)
+        return [x.value for x in n_largest]
+
+    def solution_3(self, nums: List[int], k: int) -> List[int]:
+        """
+        用python自带的Counter类
+        结果：
+        - 21/21 cases passed (116 ms)
+        - Your runtime beats 94.86 % of python3 submissions
+        - Your memory usage beats 7.35 % of python3 submissions (17.3 MB)
+        """
+        from collections import Counter
+        return [x[0] for x in Counter(nums).most_common(k)]
+
+
+class Node:
+    """堆节点对象"""
+    def __init__(self, order, value):
+        self.order = order
+        self.value = value
+
+        self.parent = None
+        self.lc = None
+        self.rc = None
+
+    def __gt__(self, other):
+        """定义 > 操作符"""
+        return self.order > other.order
+
 
 class BigRootHeap:
+    """基于数组实现的大根堆"""
+    def __init__(self):
+        self._nodes = []
 
+    @property
+    def root(self):
+        if self._nodes:
+            return self._nodes[0]
+        else:
+            return None
+
+    def get_parent_rank(self, rank: int):
+        """获取父节点"""
+        if rank > 0:
+            return (rank - 1) // 2
+        else:
+            return None
+
+    def get_left_child_rank(self, rank: int):
+        """获取左子节点"""
+        return rank * 2 + 1
+
+    def get_right_child_rank(self, rank: int):
+        """获取右子节点"""
+        return rank * 2 + 2
+
+    def push(self, node: Node):
+        """向堆添加节点"""
+        if not self._nodes:
+            self._nodes = [node]
+        else:
+            # 添加到堆的末尾
+            self._nodes.append(node)
+            # 添加新元素后调整堆，以维持堆的性质
+            self.heapify_up(len(self._nodes) - 1)
+
+    def heapify_up(self, rank: int):
+        """检查节点与其父节点是否满足大根堆的性质，如果不满足则调整"""
+        if rank == 0:
+            # 已经是根节点，不用调整
+            return
+        else:
+            parent_rank = self.get_parent_rank(rank)
+            if self._nodes[rank] > self._nodes[parent_rank]:
+                # 将节点与父节点交换
+                self._nodes[rank], self._nodes[parent_rank] = \
+                    self._nodes[parent_rank], self._nodes[rank]
+                # 递归对祖先节点进行调整，最大调整次数：logn
+                self.heapify_up(parent_rank)
+            else:
+                # 当前节点与其父节点的关系不需要调整
+                return
+
+    def pop(self):
+        """弹出当前堆顶元素"""
+        # 将末位元素交换到堆顶，取出最后一个元素，然后堆长减一
+        self._nodes[0], self._nodes[-1] = self._nodes[-1], self._nodes[0]
+        res = self._nodes.pop()
+        # 从堆顶开始调整，维持堆性质
+        self.heapify_down(0)
+
+        return res
+
+    def heapify_down(self, rank: int):
+        """检查节点与其子节点是否满足大根堆的性质，如果不满足则调整"""
+        if rank >= len(self._nodes) // 2:
+            # 当前节点已经没有子节点了
+            return
+        else:
+            lc_child_rank = self.get_left_child_rank(rank)
+            rc_child_rank = self.get_right_child_rank(rank)
+            # 如果当前节点比左子节点或右子节点小，则将其与最大的子节点交换，然后到相应的子节点位置继续调整。最大调整次数：logn
+            biggest_rank = rank
+            if self._nodes[biggest_rank] < self._nodes[lc_child_rank]:
+                biggest_rank = lc_child_rank
+            if rc_child_rank < len(self._nodes) and self._nodes[biggest_rank] < self._nodes[rc_child_rank]:
+                biggest_rank = rc_child_rank
+
+            if biggest_rank != rank:
+                self._nodes[rank], self._nodes[biggest_rank] = \
+                        self._nodes[biggest_rank], self._nodes[rank]
+                self.heapify_down(biggest_rank)
+            else:
+                return
 
 # @lc code=end
-
